b4128c89faaf7acb9fbe6f803fd4b62c2ff354d4
idanBarzellai <idanbarzi@gmail.com>
2025-01-24 13:58:30 +0200

    BGcontroller and Spawner
    aaded logic to spawner and BGcontroller class

---
+++ Assets/Scripts/BGController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BGController : MonoBehaviour
+{
+    private Spawner spawner;
+    void Start()
+    {
+        spawner = GetComponent<Spawner>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
